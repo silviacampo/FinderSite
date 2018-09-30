2075f398ca705aebd73cebf4a8fb071b8aa83337
Silvia Campo <Silvia@DESKTOP-RP9FHTN>
2018-09-30 17:26:21 -0400

    correction abort connection

--- webGDPR/Infrastructure/CustomWebSockets/CustomWebSocketManager.cs
+++ webGDPR/Infrastructure/CustomWebSockets/CustomWebSocketManager.cs
@@ -67,7 +67,28 @@ namespace webGDPR.Infrastructure.CustomWebSockets
 				await wsmHandler.HandleMessage(result, buffer, userWebSocket, wsFactory, dbContext, mapper, emailSender);
 
 				buffer = new byte[1024 * 4];
-				result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                /*
+                 2018-09-30 19:48:10,393 [42] ERROR Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware - An unhandled exception has occurred while executing the request.
+System.Net.WebSockets.WebSocketException (0x80004005): The remote party closed the WebSocket connection without completing the close handshake. ---> System.Net.WebSockets.WebSocketException (0x80004005): The remote party closed the WebSocket connection without completing the close handshake.
+   at System.Net.WebSockets.ManagedWebSocket.ThrowIfEOFUnexpected(Boolean throwOnPrematureClosure)
+   at System.Net.WebSockets.ManagedWebSocket.EnsureBufferContainsAsync(Int32 minimumRequiredBytes, Boolean throwOnPrematureClosure)
+   at System.Net.WebSockets.ManagedWebSocket.ReceiveAsyncPrivate[TWebSocketReceiveResultGetter,TWebSocketReceiveResult](Memory`1 payloadBuffer, CancellationToken cancellationToken, TWebSocketReceiveResultGetter resultGetter)
+   at System.Net.WebSockets.ManagedWebSocket.ReceiveAsyncPrivate[TWebSocketReceiveResultGetter,TWebSocketReceiveResult](Memory`1 payloadBuffer, CancellationToken cancellationToken, TWebSocketReceiveResultGetter resultGetter)
+   at webGDPR.Infrastructure.CustomWebSockets.CustomWebSocketManager.Listen(HttpContext context, CustomWebSocket userWebSocket, ICustomWebSocketFactory wsFactory, ICustomWebSocketMessageHandler wsmHandler, ApplicationDbContext dbContext, IMapper mapper, IEmailSender emailSender) in /home/silvia/Desktop/testwsgdpr/webGDPR/Infrastructure/CustomWebSockets/CustomWebSocketManager.cs:line 70
+   at webGDPR.Infrastructure.CustomWebSockets.CustomWebSocketManager.Invoke(HttpContext context, ICustomWebSocketFactory wsFactory, ICustomWebSocketMessageHandler wsmHandler, SignInManager`1 signInManager, ApplicationDbContext dbContext, IMapper mapper, IEmailSender emailSender) in /home/silvia/Desktop/testwsgdpr/webGDPR/Infrastructure/CustomWebSockets/CustomWebSocketManager.cs:line 46
+   at Microsoft.AspNetCore.Authentication.AuthenticationMiddleware.Invoke(HttpContext context)
+   at Microsoft.AspNetCore.StaticFiles.StaticFileMiddleware.Invoke(HttpContext context)
+   at Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware.Invoke(HttpContext context)
+                 */
+                try {
+                    result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                }
+                catch (WebSocketException e) {
+                    wsFactory.Remove(userWebSocket.Guid);
+                    await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+                }
+                
 			}
 			wsFactory.Remove(userWebSocket.Guid);
 			await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
