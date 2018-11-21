65e729f8e8e094418a87e0ec6d825a60a3f4789e
Silvia Campo <Silvia@DESKTOP-RP9FHTN>
2018-11-20 21:24:55 -0500

    corrections

--- webGDPR/Infrastructure/GPSFileService.cs
+++ webGDPR/Infrastructure/GPSFileService.cs
@@ -8,71 +8,76 @@ using System.Threading.Tasks;
 namespace webGDPR.Infrastructure
 {
     public class GPSFileService : HostedService
-	{
-		public const string url = "https://offline-live1.services.ublox.com/GetOfflineData.ashx?token=Tdw1rYjjLES8m8cObGyfiA;gnss=gps,glo;alm=gps,glo;period=1;resolution=2";
+    {
+        public const string url = "https://offline-live1.services.ublox.com/GetOfflineData.ashx?token=Tdw1rYjjLES8m8cObGyfiA;gnss=gps,glo;alm=gps,glo;period=1;resolution=2";
 
-		public GPSFileService()
-		{
+        public GPSFileService()
+        {
 
-		}
+        }
+        System.Timers.Timer t;
 
-		protected override async Task ExecuteAsync(System.Threading.CancellationToken cancellationToken)
-		{
-			while (!cancellationToken.IsCancellationRequested)
-			{
-				TimeSpan timeBetween = DateTime.Today.AddDays(1).AddHours(1) - DateTime.Now;
-				System.Timers.Timer t = new System.Timers.Timer();
-				t.Elapsed += T_Elapsed;
-				t.Interval = 1000 * timeBetween.TotalSeconds;
-				t.Start();
+        protected override async Task ExecuteAsync(System.Threading.CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                TimeSpan timeBetween = DateTime.Today.AddDays(1).AddHours(1) - DateTime.Now;
+                t = new System.Timers.Timer();
+                t.Elapsed += T_Elapsed;
+                t.Interval = 1000 * timeBetween.TotalSeconds;
+                t.Start();
+            }
+        }
 
-					await DownloadAndSave();
+        private void T_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            t.Stop();
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await DownloadAndSave();
+                    await Task.Delay(TimeSpan.FromDays(1));
+                }
+            });
 
 
-			}
-		}
+        }
 
-		private void T_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
-		{
-			//go and grab a new file
-			Task.Run(async () =>
-			{
-				await DownloadAndSave();
-			});
-		}
+        private async Task DownloadAndSave()
+        {
+            var htmlpath = Path.Combine(CustomPaths.GetGPSFilesPath(), "mgaoffline.ubx");
+            if (!Directory.Exists(Path.GetDirectoryName(htmlpath)))
+                Directory.CreateDirectory(Path.GetDirectoryName(htmlpath));
 
-		private async Task DownloadAndSave() {
-			var htmlpath = Path.Combine(CustomPaths.GetGPSFilesPath(), "mgaoffline.ubx");
-			if (!Directory.Exists(Path.GetDirectoryName(htmlpath)))
-				Directory.CreateDirectory(Path.GetDirectoryName(htmlpath));
+            if (File.Exists((htmlpath)))
+            {
+                File.Delete(htmlpath);
+            }
+            byte[] filebytes = await DownloadFile(url);
+            await File.WriteAllBytesAsync(htmlpath, filebytes);
+        }
 
-			if (File.Exists((htmlpath)))
-			{
-				File.Delete(htmlpath);
-			}
-			byte[] filebytes = await DownloadFile(url);
-			await File.WriteAllBytesAsync(htmlpath, filebytes);
-		}
-
-		private async Task<byte[]> DownloadFile(string url)
-		{
-			using (var client = new HttpClient())
-			{
-				try
-				{
-					using (var result = await client.GetAsync(url))
-					{
-						if (result.IsSuccessStatusCode)
-						{
-							return await result.Content.ReadAsByteArrayAsync();
-						}
-					}
-				}
-				catch (Exception ex) {
-					var test = ex.Message;
-				}
-			}
-			return null;
-		}
-	}
+        private async Task<byte[]> DownloadFile(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    using (var result = await client.GetAsync(url))
+                    {
+                        if (result.IsSuccessStatusCode)
+                        {
+                            return await result.Content.ReadAsByteArrayAsync();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    var test = ex.Message;
+                }
+            }
+            return null;
+        }
+    }
 }
