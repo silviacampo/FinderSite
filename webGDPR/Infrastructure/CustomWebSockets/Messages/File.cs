4d38cbb094f20de3bbb9c7031b7aff832c0c38ec
scampo <scampo@TS-PCL-17-12-01>
2018-12-10 12:39:26 -0500

    reduce the payload for the files

--- webGDPR/Infrastructure/CustomWebSockets/Messages/File.cs
+++ webGDPR/Infrastructure/CustomWebSockets/Messages/File.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace webGDPR.Infrastructure.CustomWebSockets.Messages
 {
 	public class File
-	{ 
+	{
+		[JsonProperty(PropertyName = "t")]
 		public int Type { get; set; }
+
+		[JsonProperty(PropertyName = "f")]
 		public string Filename { get; set; }
 	}
 }
