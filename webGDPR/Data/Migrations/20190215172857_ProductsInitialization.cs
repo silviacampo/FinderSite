fe2899a5627c4defbdd32d6086892c561a92a08b
scampo <scampo@TS-PCL-17-12-01>
2019-02-15 14:44:54 -0500

    add initial products for subscription

---
+++ webGDPR/Data/Migrations/20190215172857_ProductsInitialization.cs
@@ -0,0 +1,26 @@
+﻿//this migration is not working automatically. but it's useful to keep the info in the project
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace webGDPR.Data.Migrations
+{
+	public partial class ProductsInitialization : Migration
+	{
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+				migrationBuilder.InsertData(
+				table: "product",
+			   columns: new[] { "Model","Name","Text","Type","Price" },
+			   values: new object[] { "S-1", "Subscription-1", "Monthly Subscription", "services", 10 });
+
+				migrationBuilder.InsertData(
+				table: "product",
+				columns: new[] { "Model", "Name", "Text", "Type", "Price" },
+				values: new object[] { "SD2-1", "SubscriptionDiscount2-1", "Subscription Discount for two trackers", "services", -3 });
+
+				migrationBuilder.InsertData(
+				table: "product",
+				columns: new[] { "Model", "Name", "Text", "Type", "Price" },
+				values: new object[] { "SD3+-1", "SubscriptionDiscount3+-1", "Subscription Discount for three or more trackers", "services", -5 });
+		}
+		}
+	}
