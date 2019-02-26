900c04b01ff9570c03ad3c5a027063cd44a0133e
scampo <scampo@TS-PCL-17-12-01>
2019-02-26 14:58:53 -0500

    register add address - missing form action is static

--- webGDPR/Areas/Identity/Pages/Account/InstallApplication.cshtml.cs
+++ webGDPR/Areas/Identity/Pages/Account/InstallApplication.cshtml.cs
@@ -2,15 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using webGDPR.Data;
+using webGDPR.Models;
 
 namespace webGDPR.Areas.Identity.Pages.Account
 {
     public class InstallApplicationModel : PageModel
     {
-        public void OnGet()
+		public User UserInfo;
+
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly ApplicationDbContext _context;
+		private readonly ILogger<RegisterModel> _logger;
+
+
+		public InstallApplicationModel(
+			UserManager<ApplicationUser> userManager,
+			ApplicationDbContext context,
+			ILogger<RegisterModel> logger)
+		{
+			_userManager = userManager;
+			_context = context;
+			_logger = logger;
+
+		}
+
+		public async Task OnGetAsync()
         {
-        }
+			UserInfo = await _context.User.FirstOrDefaultAsync(u => u.OwnerID == _userManager.GetUserId(User));
+		}
     }
 }
