de104d4df89d83e4070148445d304e41188b8b32
scampo <scampo@TS-PCL-17-12-01>
2019-03-01 12:55:52 -0500

    hw stats

--- webGDPR/ViewModels/HWOverviewViewModel.cs
+++ webGDPR/ViewModels/HWOverviewViewModel.cs
@@ -20,5 +20,45 @@ namespace webGDPR.ViewModels
 
 		public List<Tuple<PetTrackingInfo, string>> PointVisited { get; internal set; }
 
+		public Device MostConnectedToDevice { get; set;}
+
+		public Base MostConnectedToBase { get; set; }
+
+		public List<BaseStats> BaseStats { get; set; }
+
+		public List<CollarStats> CollarStats { get; set; }
+
+	}
+
+	public class BaseStats
+	{
+		public Base Base { get; set; }
+		public TimeSpan DisconnectedTimeSpan { get; set; }
+		public List<Tuple<string, TimeSpan>> ConnectedToTimeSpan { get; set; }
+		public TimeSpan[] RadioTimeSpan { get; set; }
+		public double AvgRadio { get; set; }
+		public TimeSpan IsChargingTimeSpan { get; set; }
+		public bool BatteriesChargingMore75percent { get; set; }
+	}
+
+	public class CollarStats
+	{
+		public Collar Collar { get; set; }
+		public TimeSpan DisconnectedTimeSpan { get; set; }
+		public List<Tuple<string, TimeSpan>> ConnectedToTimeSpan { get; set; }
+		public TimeSpan[] RadioTimeSpan { get; set; }
+		public double AvgRadio { get; set; }
+		public TimeSpan GPSDisconnectedTimeSpan { get; set; }
+		public TimeSpan BatteryMinus25TimeSpan { get; set; }
+		public double AvgBattery { get; set; }
+		public List<PointServiceLevel> PointsServiceLevel { get; set; }
+	}
+
+	public class PointServiceLevel
+	{
+		public int Battery { get; set; }
+		public int Radio { get; set; }
+		public double Latitude { get; set; }
+		public double Longitude { get; set; }
 	}
 }
