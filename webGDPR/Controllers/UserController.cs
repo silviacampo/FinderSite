a1078887eb27701aba2e62c599bde5dc038a9df0
scampo <scampo@TS-PCL-17-12-01>
2019-03-22 14:46:36 -0400

    timeline

--- webGDPR/Controllers/UserController.cs
+++ webGDPR/Controllers/UserController.cs
@@ -76,9 +76,21 @@ namespace webGDPR.Controllers
 
 		[Authorize]
 		public async Task<IActionResult> ConnectionTimeline() {
+			List<Infrastructure.TimelineItem> list = new List<Infrastructure.TimelineItem>();
 			User user = await _context.User.Include(b => b.Bases).ThenInclude(b => b.LastStatus).ThenInclude(c => c.DeviceConnectedTo).Include(c => c.Collars).ThenInclude(b => b.LastStatus).ThenInclude(c => c.BaseConnectedTo).Include(d => d.Devices).Include(d => d.Pets).ThenInclude(pe => pe.LastMode).Include(d => d.Pets).ThenInclude(pe => pe.LastCollar).Include(d => d.Pets).ThenInclude(pe => pe.LastTrackingInfo).FirstOrDefaultAsync(u => u.OwnerID == _userManager.GetUserId(User));
+			List<string> devicesID = user.Devices.Select(l=>l.DeviceId).ToList();
+			List<DeviceLog> logs = await _context.DeviceLog.Where(l=> devicesID.Contains(l.DeviceId)).OrderByDescending(l=>l.CreationDate).ToListAsync();
+			foreach (var log in logs) {
+				list.Add(new Infrastructure.TimelineItem() {
+					ItemDate = log.CreationDate,
+					ItemLeftTitle = user.Devices.FirstOrDefault(l => l.DeviceId == log.DeviceId).GetName,
+					ItemMessage = log.Reason,
+					ItemMore = log.Message,
+					Orientation = (log.Reason.Contains("WebSocket") ? Infrastructure.TimelineItemOrientation.left: Infrastructure.TimelineItemOrientation.right)
 
-			return View(user);
+				});
+			}
+			return View(list);
 		}
 
 		[Authorize]
---
+++ webGDPR/Infrastructure/TimelineItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace webGDPR.Infrastructure
+{
+	public class TimelineItem
+	{
+		public DateTime ItemDate { get; set; }
+		public string ItemLeftTitle { get; set; }
+		public string ItemMessage { get; set; }
+		public string ItemMore { get; set; }
+		public TimelineItemOrientation Orientation { get; set; }
+	}
+
+	public enum TimelineItemOrientation
+	{
+		left,
+		right
+	}
+}
