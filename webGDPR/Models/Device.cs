b79db89207ae51cefa5583906d3b3282662f3cf9
scampo <scampo@TS-PCL-17-12-01>
2019-03-29 09:14:22 -0400

    edit device format and labels

--- webGDPR/Models/Device.cs
+++ webGDPR/Models/Device.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace webGDPR.Models
 {
@@ -7,11 +8,14 @@ namespace webGDPR.Models
 		public string DeviceId { get; set; }
 		public string Type { get; set; }
 		public string Platform { get; set; }
+		[DisplayName("Device Name")]
 		public string Name { get; set; }
 		public string Model { get; set; }
 		public string Manufacturer { get; set; }
+		[DisplayName("OS Version")]
 		public string OSVersion { get; set; }
 
+		[DisplayName("Displayed Name (Optional, used in our site and application replacing the Device Name)")]
 		public string AliasName { get; set; }
 		public bool Banned { get; set; }
 		public bool IsLogging { get; set; }
